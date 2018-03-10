0730405f9c101ad1656f53de80c9475a26752c59
Corentin Jabot <corentinjabot@gmail.com>
2018-03-11 00:53:52 +0100

    Implement iterating over replacements

--- include/ucd/decomposition.h
+++ include/ucd/decomposition.h
@@ -31,17 +31,67 @@ constexpr char32_t canonical_bitmask = 0x80000000;
 
 class rule {
 public:
-    struct alignas(alignof(char32_t*)) replacement {
+    class replacement {
+        const char32_t* const m_data;
+        friend class rule;
+
+    public:
+        replacement(const char32_t* const data) : m_data(data) {}
+
         char32_t codepoint() const {
             return (*m_data) & ~(composed_bitmask);
         }
 
-        bool need_recursion() const {
-            return (*m_data & composed_bitmask);
+        bool is_composed() const {
+            return (*m_data) & composed_bitmask;
         }
+    };
+    class replacements_view : public ranges::v3::view_facade<replacements_view, ranges::finite> {
+    public:
+        replacements_view(const char32_t* start, const rule_size_t rule_size) :
+            m_start(start),
+            m_rule_size(rule_size) {}
 
     private:
-        const char32_t* const m_data;
+        const char32_t* m_start;
+        const rule_size_t m_rule_size;
+        friend ranges::v3::range_access;
+
+        struct cursor {
+        private:
+            const char32_t* m_pos;
+
+        public:
+            cursor() = default;
+            cursor(const char32_t* data) : m_pos(data) {}
+            replacement read() const {
+                return replacement(m_pos);
+            }
+            bool equal(cursor const& other) const {
+                return m_pos == other.m_pos;
+            }
+            bool equal(ranges::v3::default_sentinel) const {
+                return *m_pos == nil_cp;
+            }
+            void next() {
+                m_pos++;
+            }
+            void prev() {
+                m_pos--;
+            }
+            std::ptrdiff_t distance_to(cursor const& other) const {
+                return (other.m_pos - m_pos);
+            }
+            void advance(std::ptrdiff_t n) {
+                m_pos += n;
+            }
+        };
+        cursor begin_cursor() const {
+            return {m_start};
+        }
+        cursor end_cursor() const {
+            return {m_start + m_rule_size};
+        }
     };
 
     rule(rule_size_t rule_size, const char32_t* const data) :
@@ -54,15 +104,12 @@ public:
             return m_codepoint;
         return (*m_data) & ~(canonical_bitmask);
     }
-    ranges::iterator_range<const replacement*> make_range(const char32_t* const first,
-                                                          rule_size_t count) {
-        return {reinterpret_cast<const replacement*>(first),
-                reinterpret_cast<const replacement*>(first + count)};
-    }
-
 
     auto replacements() {
-        return make_range(m_data + 1, m_rule_size);
+        if(m_rule_size == 0) {
+            return replacements_view(&m_codepoint, 1);
+        }
+        return replacements_view(m_data + 1, m_rule_size);
     }
 
 private:
@@ -73,7 +120,7 @@ private:
     };
 };
 
-bool operator<(const rule& r, char32_t codepoint) {
+inline bool operator<(const rule& r, char32_t codepoint) {
     return r.codepoint() < codepoint;
 }
 
@@ -93,28 +140,30 @@ private:
     struct cursor {
     private:
         rule_size_t m_rule_size;
-        const char32_t* m_pos;
+        const char32_t* m_curser_pos;
 
     public:
         cursor() = default;
-        cursor(rule_size_t rule_size, const char32_t* data) : m_rule_size(rule_size), m_pos(data) {}
+        cursor(rule_size_t rule_size, const char32_t* data) :
+            m_rule_size(rule_size),
+            m_curser_pos(data) {}
         rule read() const {
-            return rule(m_rule_size, m_pos);
+            return rule(m_rule_size, m_curser_pos);
         }
         bool equal(cursor const& other) const {
-            return m_pos == other.m_pos;
+            return m_curser_pos == other.m_curser_pos;
         }
         void next() {
-            m_pos += factor();
+            m_curser_pos += factor();
         }
         void prev() {
-            m_pos -= factor();
+            m_curser_pos -= factor();
         }
         std::ptrdiff_t distance_to(cursor const& other) const {
-            return (other.m_pos - m_pos) / factor();
+            return (other.m_curser_pos - m_curser_pos) / factor();
         }
         void advance(std::ptrdiff_t n) {
-            m_pos += n * factor();
+            m_curser_pos += n * factor();
         }
 
         std::make_unsigned_t<size_t> factor(size_t n = 1) const {
@@ -125,7 +174,7 @@ private:
         return {m_rule_size, m_pos};
     }
     cursor end_cursor() const {
-        return {m_rule_size, m_pos + m_rule_count * (m_rule_size + 1) + 1};
+        return {m_rule_size, m_pos + m_rule_count * (m_rule_size + 1)};
     }
 };
 
@@ -162,6 +211,22 @@ namespace unicode {
 template<typename Container>
 void decompose(char32_t codepoint, Container& c) {
     auto rule = details::find_rule_for_code_point(codepoint);
+    for(auto&& replacement : rule.replacements()) {
+        if(replacement.is_composed()) {
+            decompose(replacement.codepoint(), c);
+            continue;
+        }
+        c.push_back(replacement.codepoint());
+    }
 }
+inline std::u32string decomposed(const std::u32string& in) {
+    std::u32string out;
+    for(auto& codepoint : in) {
+        decompose(codepoint, out);
+    }
+    // out.push_back('\0');
+    return out;
+}
+
 
 }    // namespace unicode
